f5e1cb430525cdff722d132160c3a55da18c4984
Filipe Paiva <filipe.p@soltroiavillas.com>
2019-02-18 21:57:11 +0200

    another migration and after validation geocoding in model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_195423) do
+ActiveRecord::Schema.define(version: 2019_02_18_195536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_18_195423) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
