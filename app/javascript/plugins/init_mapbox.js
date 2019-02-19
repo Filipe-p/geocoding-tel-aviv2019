a5edacb2705a3a5a1b12ad0c061bff5c38cb5447
Filipe Paiva <filipe.p@soltroiavillas.com>
2019-02-19 09:02:23 +0200

    working map with bananas

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,46 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { duration:12000, padding: 170, maxZoom: 11 });
+};
+
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/filipecowstore/cjsazm1lo01nz1fmphiqj02iz'
+    });
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
+  const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // <-- add this
+  new mapboxgl.Marker(element)
+    .setLngLat([marker.lng, marker.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML(marker.infoWindow.content))
+    .addTo(map);
+
+
+  });
+
+  fitMapToMarkers(map, markers);
+
+
+  }
+};
+
+export { initMapbox };
